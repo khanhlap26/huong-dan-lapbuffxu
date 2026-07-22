39e87b485d982e14409bfacac5b98784d6c5acfa
khanhlap26 <108079472+khanhlap26@users.noreply.github.com>
2026-07-22 14:25:10 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+
+const MAIN_WEBSITE_URL = "https://lapbuffxu.site";
+
+const tools = [
+  {
+    name: "KTOOLS MOBILE",
+    video: "https://drive.google.com/file/d/1XwkukR1UMhOx5UuEMZfLiwtaUsKUMafH/view?usp=drivesdk",
+    download: "https://www.mediafire.com/file/io7acp8x4ekozrj/Multi_App_3.6.11_Vippro.apk/file",
+    note: "LÀM THEO VIDEO ĐỪNG COI LƯỚT."
+  },
+  {
+    name: "KTOOLS GIẢ LẬP",
+    video: "https://drive.google.com/file/d/1Xn4tQWoCqoDuieD3y1mbRHro0em24jls/view?usp=drive_link",
+    download: "https://www.mediafire.com/file/7otpl5xsm34o194/module-ktn.zip/file",
+    note: "LÀM THEO VIDEO ĐỪNG COI LƯỚT."
+  },
+  {
+    name: "KDG MOD",
+    video: "https://drive.google.com/file/d/1XwkukR1UMhOx5UuEMZfLiwtaUsKUMafH/view?usp=drivesdk",
+    download: "https://www.mediafire.com/file/0yaovd3eth4dm20/Kdg+Mod+Play+Together.apk/file",
+    note: "LÀM THEO VIDEO ĐỪNG COI LƯỚT."
+  },
+    {
+    name: "KDG MOD Giả Lập ld9",
+    video: "https://drive.google.com/file/d/1zGRWtvwc5Top6B4X8XR9LjbpoIvZjfKs/view?usp=drivesdk",
+    download: "https://www.mediafire.com/folder/uhuqw8s73kbe7/KDG-PC",
+    note: "LÀM THEO VIDEO ĐỪNG COI LƯỚT."
+  },
+
+];
